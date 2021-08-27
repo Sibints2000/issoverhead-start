446217947fec70722a3b272e95b7479decb5ce9b
Sibints2000 <sibints2000@gmail.com>
2021-08-27 22:01:27 +0530

    Ninth Commit

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ def is_night():
         "lng": MY_LONG,
         "formatted": 0,
     }
-
     response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
     response.raise_for_status()
     data = response.json()
