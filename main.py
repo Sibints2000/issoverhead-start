ae4e10e72e7aabb331d2454181ea9c6884dacc1b
Sibints2000 <sibints2000@gmail.com>
2021-08-27 22:05:07 +0530

    Fifteenth Commit

--- main.py
+++ main.py
@@ -35,7 +35,5 @@ def is_night():
     if time_now >= sunset or time_now <= sunrise:
         return True
 
-# If the ISS is close to my current position
-# and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
