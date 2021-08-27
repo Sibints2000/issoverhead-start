ab2b2f37fc5411fb20ae62aa3c601a75222973a1
Sibints2000 <sibints2000@gmail.com>
2021-08-27 22:00:56 +0530

    Eighth Commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 51.507351  # Your latitude
+MY_LONG = -0.127758  # Your longitude
 
 
 def is_iss_overhead():
